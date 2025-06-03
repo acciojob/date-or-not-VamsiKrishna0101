81d26bd8a8970a37ca4351758924821a86d3dc4b
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-03 17:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var isDate = function (input) {
   //   write your code here
+	const regex = /^(19|20)\d\d[-\/](0[1-9]|1[0-2])[-\/](0[1-9]|[12][0-9]|3[01])$/;
+
+return regex.test(input)
 };
 
 // Do not change the code below.
